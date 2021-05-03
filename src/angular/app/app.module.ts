54f79f2b9edd2fcd375b20f29f6156c6886f1aea
Craig McKeachie <craigdmckeachie@gmail.com>
2021-05-03 17:31:59 -0400

    step 4 completed

--- src/angular/app/app.module.ts
+++ src/angular/app/app.module.ts
@@ -2,11 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UpgradeModule } from '@angular/upgrade/static';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, UpgradeModule],
   providers: [],
-  bootstrap: [AppComponent],
+  // bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) {}
+
+  ngDoBootstrap() {
+    this.upgrade.bootstrap(document.body, ['phonecatApp'], { strictDi: true });
+  }
+}
