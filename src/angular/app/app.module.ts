4a8567f61c877c16395a93cd098a8e1783545623
Craig McKeachie <craigdmckeachie@gmail.com>
2021-05-03 17:39:10 -0400

    step 5 completed

--- src/angular/app/app.module.ts
+++ src/angular/app/app.module.ts
@@ -2,11 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { UpgradeModule } from '@angular/upgrade/static';
+import { UpgradeModule, downgradeComponent } from '@angular/upgrade/static';
+import { WidgetComponent } from './widget/widget.component';
+
+declare var angular: any;
+
+angular
+  .module('phonecatApp')
+  .directive('appWidget', downgradeComponent({ component: WidgetComponent }));
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WidgetComponent],
   imports: [BrowserModule, UpgradeModule],
+  entryComponents: [WidgetComponent],
   providers: [],
   // bootstrap: [AppComponent],
 })
---
+++ src/angular/app/widget/widget.component.ts
@@ -0,0 +1,6 @@
+import { Component } from "@angular/core";
+@Component({
+  selector: "app-widget",
+  template: ` <h3>Angular Widget</h3> `
+})
+export class WidgetComponent {}
